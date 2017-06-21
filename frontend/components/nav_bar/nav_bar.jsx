27f531b2b8ab33798eb394a4c2092b89c1a2d5cf
Lora Lee <loraalee@gmail.com>
2017-06-20 20:23:49 -0400

    Add nav bar items and icons

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -1,23 +1,50 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const sessionLinks = () => (
-  <nav className="login-signup">
-    <Link to="/login">Login</Link>
-    &nbsp;or&nbsp;
-    <Link to="/signup">Sign up!</Link>
-  </nav>
-);
+const rightNav = (currentUser) => {
+  if (currentUser) {
+    return <li>Profile Icon</li>;
+  } else {
+    return (
+      <div>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/signup">Sign up</Link>
+        </li>
+      </div>
+    );
+  }
+};
 
-const personalNavBar = (currentUser, logout) => (
-	<hgroup className="header-group">
-    <h2 className="logo">QUICKSTARTER</h2>
-    <button className="header-button" onClick={logout}>Log Out</button>
-	</hgroup>
-);
+const NavBar = ({ currentUser }) => {
+  return (
+    <header className="nav-bar">
+      <nav className="left-nav">
+        <ul>
+          <li id="explore-link">
+            <i className="fa fa-compass"></i>
+            <span>Explore</span>
+          </li>
+          <li id="start-project-link">
+            <Link to="/projects/new">Start a project</Link>
+          </li>
+        </ul>
+      </nav>
 
-const NavBar = ({ currentUser, logout }) => (
-  currentUser ? personalNavBar(currentUser, logout) : sessionLinks()
-);
+      <nav className="nav-logo">QUICKSTARTER</nav>
+
+      <nav className="right-nav">
+        <ul>
+          <li>
+            <i className="fa fa-search"></i>
+          </li>
+          {rightNav(currentUser)}
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default NavBar;
