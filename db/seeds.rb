c5152a9dd9260241005ef14dde194d3ae32716df
Lora Lee <loraalee@gmail.com>
2017-06-27 14:52:08 -0400

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,32 +7,18 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.destroy_all
-User.create(name: "Guest", email: "guest@guest.com", password: "password")
-User.create(name: "Lora Lee", email: "loraalee@gmail.com", password: "password")
-User.create(name: "Bob", email: "bob@bob.com", password: "password")
-
-Project.destroy_all
-Project.create!(
-  title: "Violini - The World's Smallest Violin",
-  description: "World's smallest violin",
-  end_date: DateTime.now,
-  funding_goal: 1000,
-  creator_id: 1,
-  category_id: 1
-)
-
-Project.create!(
-  title: "Pegasus - The Flying Car",
-  description: "A car that flies!",
-  end_date: DateTime.now,
-  funding_goal: 50000,
-  creator_id: 2,
-  category_id: 2
-)
+users = User.create!([
+  {name: "Guest", email: "guest@guest.com", password: "password"},
+  {name: "Lora Lee", email: "loraalee@gmail.com", password: "password"},
+  {name: "Bob", email: "bob@bob.com", password: "password"}
+])
+# User.create(name: "Guest", email: "guest@guest.com", password: "password")
+# User.create(name: "Lora Lee", email: "loraalee@gmail.com", password: "password")
+# User.create(name: "Bob", email: "bob@bob.com", password: "password")
 
 Category.destroy_all
-Category.create(name: "Art")
-Category.create(name: "Crafts")
+a = Category.create(name: "Art")
+b = Category.create(name: "Crafts")
 Category.create(name: "Design")
 Category.create(name: "Fashion")
 Category.create(name: "Film & Video")
@@ -40,3 +26,22 @@ Category.create(name: "Food")
 Category.create(name: "Music")
 Category.create(name: "Technology")
 Category.create(name: "Theater")
+
+Project.destroy_all
+Project.create!(
+  title: "Violini - The World's Smallest Violin",
+  description: "World's smallest violin",
+  end_date: DateTime.now,
+  funding_goal: 1000,
+  creator_id: users[0].id,
+  category_id: a.id
+)
+
+Project.create!(
+  title: "Pegasus - The Flying Car",
+  description: "A car that flies!",
+  end_date: DateTime.now,
+  funding_goal: 50000,
+  creator_id: users[1].id,
+  category_id: b.id
+)
