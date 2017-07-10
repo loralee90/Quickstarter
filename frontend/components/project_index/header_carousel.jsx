2a101300f0cfb7f89ec475e87be28799d5fae33f
Lora Lee <loraalee@gmail.com>
2017-07-10 17:56:08 -0400

    Add favicon

--- frontend/components/project_index/header_carousel.jsx
+++ frontend/components/project_index/header_carousel.jsx
@@ -33,33 +33,33 @@ const HeaderCarousel = () => {
 
 export default HeaderCarousel;
 
-// <div className="header-carousel-slide">
-//   <h1>Creators thrive here.</h1>
-//   <h2>Quickstarter creators pursue bold ideas on their own terms. Start a project today.</h2>
-//   <Link to="/projects/new"><button>Start project</button></Link>
-//   <img src="http://abduzeedo.com/sites/default/files/styles/home_cover/public/originals/8fcaca8a-eab8-9b7a-510a-87b412469a88.jpg?itok=-w7GCqqx" />
-// </div>
-//
-// <div className="header-carousel-slide">
-//   <h1>Unbounded</h1>
-//   <h2>A documentary following an adventure into Patagonia for exploration and conservation.</h2>
-//   <Link to={`/projects/${66}`}><button>View project</button></Link>
-//   <img src="http://coresites-cdn.factorymedia.com/mpora_new/wp-content/uploads/2016/09/tny-14.jpg" />
-// </div>
-//
-// <div className="header-carousel-slide">
-//   <h1>Tiny Telephone Oakland</h1>
-//   <h2>Help one of the busiest recording studios in California</h2>
-//   <Link to={`/projects/${66}`}><button>View project</button></Link>
-//   <img src="http://s3.amazonaws.com/quickstarter-dev/projects/images/000/000/102/original/tiny_telephone.jpg?1498803014" />
-// </div>
-//
-// <div className="header-carousel-slide">
-//   <h1>Rise Grand Rapids</h1>
-//   <h2>A documentary following an adventure into Patagonia for exploration and conservation.</h2>
-//   <Link to={`/projects/${66}`}><button>View project</button></Link>
-//   <img src="http://s3.amazonaws.com/quickstarter-dev/projects/images/000/000/103/original/grand_rapids.png?1498803014" />
-// </div>
+  // <div className="header-carousel-slide">
+  //   <h1>Creators thrive here.</h1>
+  //   <h2>Quickstarter creators pursue bold ideas on their own terms. Start a project today.</h2>
+  //   <Link to="/projects/new"><button>Start project</button></Link>
+  //   <img src="http://abduzeedo.com/sites/default/files/styles/home_cover/public/originals/8fcaca8a-eab8-9b7a-510a-87b412469a88.jpg?itok=-w7GCqqx" />
+  // </div>
+  //
+  // <div className="header-carousel-slide">
+  //   <h1>Unbounded</h1>
+  //   <h2>A documentary following an adventure into Patagonia for exploration and conservation.</h2>
+  //   <Link to={`/projects/${66}`}><button>View project</button></Link>
+  //   <img src="http://coresites-cdn.factorymedia.com/mpora_new/wp-content/uploads/2016/09/tny-14.jpg" />
+  // </div>
+  //
+  // <div className="header-carousel-slide">
+  //   <h1>Tiny Telephone Oakland</h1>
+  //   <h2>Help one of the busiest recording studios in California</h2>
+  //   <Link to={`/projects/${66}`}><button>View project</button></Link>
+  //   <img src="http://s3.amazonaws.com/quickstarter-dev/projects/images/000/000/102/original/tiny_telephone.jpg?1498803014" />
+  // </div>
+  //
+  // <div className="header-carousel-slide">
+  //   <h1>Rise Grand Rapids</h1>
+  //   <h2>A documentary following an adventure into Patagonia for exploration and conservation.</h2>
+  //   <Link to={`/projects/${66}`}><button>View project</button></Link>
+  //   <img src="http://s3.amazonaws.com/quickstarter-dev/projects/images/000/000/103/original/grand_rapids.png?1498803014" />
+  // </div>
 //
 // <div className="header-carousel-slide">
 //   <h1>Oakly</h1>
