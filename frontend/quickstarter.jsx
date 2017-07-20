1d3662ffdfc5d2429a4df819d042e8cf37903750
Lora Lee <loraalee@gmail.com>
2017-07-20 10:58:16 -0400

    Remove things from window

--- frontend/quickstarter.jsx
+++ frontend/quickstarter.jsx
@@ -16,10 +16,6 @@ document.addEventListener('DOMContentLoaded', () => {
     store = configureStore();
   }
 
-  window.store = store;
-  window.dispatch = store.dispatch;
-  window.getState = store.getState;
-
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
 });
